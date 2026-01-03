54c625e20881aff93821c5e2914edaf838cb910f
Abdulrhman <abdulrhman@Abdulrhmans-MacBook-Pro.local>
2026-01-02 22:57:38 -0800

    fix: satisfy typescript compiler for api secret

--- app/actions/image-upload.ts
+++ app/actions/image-upload.ts
@@ -38,7 +38,7 @@ export async function getCloudinarySignature() {
                 timestamp,
                 folder,
             },
-            process.env.CLOUDINARY_API_SECRET
+            apiSecret! // Verified above
         );
 
         return {
